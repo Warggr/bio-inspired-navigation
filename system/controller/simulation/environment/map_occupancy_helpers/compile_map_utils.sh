381448733bcacb490d191f85eb97b4ccb413b7c0
Pierre Ballif <pierre.ballif@tum.de>
2024-04-23 13:20:57 +0200

    Makefile for compiling pybind modules

--- system/controller/simulation/environment/map_occupancy_helpers/compile_map_utils.sh
+++
@@ -1,8 +0,0 @@
-g++ -O3 -Wall -fPIC -shared -std=c++11 \
-    map_utils.cpp \
-    -o map_utils_cpp.dylib \
-    -static-libstdc++ \
-    -undefined dynamic_lookup \
-    -Wl,-rpath,"\$ORIGIN" \
-    $(python3 -m pybind11 --includes) \
-    $@
