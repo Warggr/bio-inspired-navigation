Fix: correct formula from Camillo

system/bio_model/bc_network/HDCActivity.py
@@ -17,5 +17,7 @@ def headingCellsActivityTraining(heading : Angle) -> p.HeadingCellActivity:
 
     tuning_vector = np.linspace(0, 2 * math.pi, p.nrHDC)     # nrHdc = 100  np.arange(0, 2 * math.pi, hdRes)
     # normal gaussian for hdc activity profile
-    activity_vector = np.exp( ((heading_vector - tuning_vector) / 2 * (math.pow(sig, 2)) ** 2) ) * amp
+    # activity_vector = np.exp( -((heading_vector - tuning_vector) / 2 * (math.pow(sig, 2)) ** 2) ) * amp
+    activity_vector = amp * np.exp(-np.power((heading_vector - tuning_vector) / 2 * sig**2, 2))
+
     return np.around(activity_vector, 5)