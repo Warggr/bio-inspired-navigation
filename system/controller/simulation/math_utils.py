fix: numerical instability

system/controller/simulation/math_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 from typing import Tuple
 
-from system.types import Vector2D
+from system.types import Vector2D, Angle
 
 def vectors_in_one_direction(v1 : Vector2D, v2 : Vector2D) -> bool:
     dot_product = np.dot(v1, v2)
@@ -22,7 +22,7 @@ def intersect(p1 : Vector2D, v1 : Vector2D, p2 : Vector2D, v2 : Vector2D) -> boo
     # Check if the intersection is in the direction of both rays
     return t >= 0 and u >= 0
 
-def compute_angle(vec_1 : Vector2D, vec_2 : Vector2D):
+def compute_angle(vec_1 : Vector2D, vec_2 : Vector2D) -> Angle:
     length_vector_1 = np.linalg.norm(vec_1)
     length_vector_2 = np.linalg.norm(vec_2)
     if length_vector_1 == 0 or length_vector_2 == 0:
@@ -30,6 +30,7 @@ def compute_angle(vec_1 : Vector2D, vec_2 : Vector2D):
     unit_vector_1 = vec_1 / length_vector_1
     unit_vector_2 = vec_2 / length_vector_2
     dot_product = np.dot(unit_vector_1, unit_vector_2)
+    dot_product = np.clip(dot_product, -1, 1) # it should be already, but due to numerical instabilities it might be e.g. -1.00002
     angle = np.arccos(dot_product)
 
     vec = np.cross([vec_1[0], vec_1[1], 0], [vec_2[0], vec_2[1], 0])