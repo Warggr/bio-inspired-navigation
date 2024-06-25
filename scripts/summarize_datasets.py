960caae0ef49e103189599612a9a4a7f99ba42f4
Pierre Ballif <bpi@in.tum.de>
2024-06-25 11:13:18 +0200

    feat: better, faster summarize_datasets

--- scripts/summarize_datasets.py
+++ scripts/summarize_datasets.py
@@ -2,19 +2,33 @@
 from pathlib import Path
 import h5py
 import numpy as np
+import sys
+from tqdm import tqdm
 
-p = Path('system/controller/reachability_estimator/data/reachability')
-datasets = p.glob('dataset*.hd5*')
-datasets = filter(lambda f: not f.is_symlink(), datasets)
+if len(sys.argv) > 1:
+    datasets = sys.argv[1:]
+else:
+    p = Path('system/controller/reachability_estimator/data/reachability')
+    datasets = p.glob('dataset*.hd5*')
+    datasets = filter(lambda f: not f.is_symlink(), datasets)
 for d in datasets:
     try:
+        print(f"{d}:", end='\t')
         f = h5py.File(d)
         dset = f['positions']
         dlength = len(dset)
-        n1 = np.zeros((len(dset),), dtype=dset.dtype)
-        dset.read_direct(n1)
-        reached = sum(n1['reached'])
         dattrs = ','.join(map(lambda keyval: f"{keyval[0]}={keyval[1]}", f.attrs.items()))
-        print(f"{d}:\t{dlength} / {reached} reached ({reached/dlength:.1%})\t{dattrs}")
+        print(f"size {dlength}\t{dattrs}")
+        BLOCK_SIZE=10000
+        #n1 = np.zeros((BLOCK_SIZE,), dtype=dset.dtype)
+        reached = 0
+        for i in (bar := tqdm(range(0, len(dset), BLOCK_SIZE))):
+            #dset.read_direct(n1, np.s_[i*BLOCK_SIZE:(i+1)*BLOCK_SIZE])
+            n1 = dset[i*BLOCK_SIZE:(i+1)*BLOCK_SIZE]
+            reached += sum(n1['reached'])
+            bar.set_description(f"        {(i+1)*BLOCK_SIZE} / {reached} reached ({reached/dlength:.1%})")
+        #print(f"\t{dlength} / {reached} reached ({reached/dlength:.1%})")
     except BlockingIOError:
-        print(f"{d}: Couldn't open dataset")
+        print("Couldn't open dataset")
+    except OSError:
+        print("Couldn't read dataset")
