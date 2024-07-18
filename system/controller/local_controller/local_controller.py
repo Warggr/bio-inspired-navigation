fix: angles are in radians

system/controller/local_controller/local_controller.py
@@ -73,12 +73,13 @@ class FajenObstacleAvoidance:
         angle = robot.position_and_angle[1] + self.angle_velocity * robot.env.dt
         return np.array([ np.cos(angle), np.sin(angle) ])
 
+
 class ObstacleAvoidance:
     def __init__(
         self,
         combine = 1.5,
         num_ray_dir = 21,
-        tactile_cone = 120,
+        tactile_cone = np.radians(120),
         ray_length = 1,
         follow_walls = False,
     ):

system/controller/simulation/pybullet_environment.py
@@ -683,7 +683,7 @@ class Robot:
 
     def calculate_simple_normal_vector(self, lidar_data: Optional[tuple[LidarReading, list[Vector2D]]] = None) -> Vector2D:
         if lidar_data is None:
-            lidar_data = self.env.lidar(tactile_cone=120, num_ray_dir=21, blind_spot_cone=0, agent_pos_orn=self.lidar_sensor_position)
+            lidar_data = self.env.lidar(tactile_cone=np.radians(120), num_ray_dir=21, blind_spot_cone=0, agent_pos_orn=self.lidar_sensor_position)
         lidar, hit_points = lidar_data
         hit_points = np.array(hit_points)
         hit_points = hit_points[np.array(lidar.distances) != -1]